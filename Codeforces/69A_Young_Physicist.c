0576434a29563a576fa688ef5661c764b7d54f31
TSulfat <156692979+TSulfat@users.noreply.github.com>
2026-01-11 23:05:55 +0600

    Add solution for Codeforces problem 69A

---
+++ Codeforces/69A_Young_Physicist.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,sum1=0,sum2=0,sum3=0;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+        {
+            int a,b,c;
+            scanf("%d%d%d",&a,&b,&c);
+            sum1=sum1+a;
+            sum2+=b;
+            sum3+=c;
+
+        }
+        if(sum1==0 && sum2==0 &&sum3==0)
+        {
+            printf("YES\n");
+        }
+        else
+        {
+            printf("NO\n");
+        }
+
+        return 0;
+}
